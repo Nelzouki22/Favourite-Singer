3c300be96ea81ed87a2a49d615de28ddaeb11e0a
NADIR ELZOUKI <elzoukigroup2018@gmail.com>
2024-09-06 19:53:38 -0400

    Add project files.

---
+++ Favourite Singer.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    unordered_map<long long, int> singerCount;
+    long long singer;
+    int maxCount = 0;
+
+    // قراءة الأغاني وحساب عدد الأغاني لكل مغني
+    for (int i = 0; i < N; i++) {
+        cin >> singer;
+        singerCount[singer]++;
+        if (singerCount[singer] > maxCount) {
+            maxCount = singerCount[singer];
+        }
+    }
+
+    // حساب عدد المغنيين المفضلين (الذين لديهم العدد الأكبر من الأغاني)
+    int favouriteSingers = 0;
+    for (const auto& pair : singerCount) {
+        if (pair.second == maxCount) {
+            favouriteSingers++;
+        }
+    }
+
+    // طباعة النتيجة
+    cout << favouriteSingers << endl;
+
+    return 0;
+}
